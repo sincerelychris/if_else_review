2031ec084be46964d44e69780b3547baf6f1a140
Chris <96923618+sincerelychris@users.noreply.github.com>
2024-03-16 11:34:57 -0500

    Add files via upload

---
+++ main-2.py
@@ -0,0 +1,25 @@
+#comparisons
+# 
+# EQUAL ->                          ==
+# NOT EQUAL ->                      != 
+# GREATER THAN ->                   >
+# LESS THAN ->                      <
+# GREATER THAN OR EQUAL ->          >=
+# LESS THAN OR EQUAL ->             <=
+# OBJECT IDENTITY ->                is
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+if a > 1:
+  print("A is greater than 1")
+if b > 1:
+  print("B is greater than 1")
+if c > 1:
+  print("C is greater than 1")
+if d > 1:
+  print("D is greater than 1")
+else:
+  print("ERROR")
